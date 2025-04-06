f591ab4046cd7c37a267c1a5388fee7a34555e2e
alexk101 <kiefera@ornl.gov>
2025-04-06 16:58:04 -0400

    New files from ubuntu

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,16 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+source $HOME/.config/fish/cachy_config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+set -gx FORGE_ROOT /home/pwk/.miniforge
+source $FORGE_ROOT/etc/fish/conf.d/conda.fish
+source $FORGE_ROOT/etc/fish/conf.d/mamba.fish
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/alexk101/.miniforge3/bin/conda
-    eval /home/alexk101/.miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/alexk101/.miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/alexk101/.miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/alexk101/.miniforge3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/pwk/.lmstudio/bin
+starship init fish | source
 
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba shell init' !!
-set -gx MAMBA_EXE "/home/alexk101/.miniforge3/bin/mamba"
-set -gx MAMBA_ROOT_PREFIX "/home/alexk101/.local/share/mamba"
+set -gx MAMBA_EXE "/home/pwk/.miniforge/bin/mamba"
+set -gx MAMBA_ROOT_PREFIX "/home/pwk/.local/share/mamba"
 $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 # <<< mamba initialize <<<
